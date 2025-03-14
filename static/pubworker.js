40bab50f6ad8f74102102654ab3492fc9a7896a9
Linh <linh@belo.im>
2025-03-14 08:27:24 +0000

    clean up

--- static/pubworker.js
+++ static/pubworker.js
@@ -1,20 +1,6 @@
 
-// self.onmessage = async function(e) {
-//     let track = e.data.track;
-//     console.log(e.data);
-//     const trackProcessor = new MediaStreamTrackProcessor(track);
-//     const reader = trackProcessor.readable.getReader();
-//     while (true) {
-//         const result = await reader.read();
-//         if (result.done) break;
-//         const frame = result.value;
-//         self.postMessage(frame);
-//     }
-// }
 self.onmessage = async ({data: {track}}) => {
     console.log(track);
-    // const vtg = new VideoTrackGenerator();
-    // self.postMessage({track: vtg.track}, [vtg.track]);
     const trackProcessor = new MediaStreamTrackProcessor({track});
     const reader = trackProcessor.readable.getReader();
     while (true) {
@@ -23,5 +9,4 @@ self.onmessage = async ({data: {track}}) => {
         const frame = result.value;
         self.postMessage(frame);
     }
-    // await mstp.readable.pipeThrough(new TransformStream({transform})).pipeTo(vtg.writable);
-  };
+};
